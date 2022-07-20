343e8cd66ba90c90c78ff21f1e1ab26eed21d3bd
ajazzv13 <ajazzv13@gmail.com>
2022-07-20 21:50:49 +0200

    Początek

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+<div id="wrapper">
+ <div id='player_wrapper'>
+  <video
+  ref = "videoPlayer"
+  id='video_player'>
+   <source src='@/assets/filmik.mp4' type='video/mp4'>
+  </video>
+  <div id='player_controls'>
+
+   <input v-if="isPlaying" type="image" 
+   :src="pauseButtonImage" 
+   @click="toggle_vid" id="play_button">
+   <input v-else type="image" 
+   :src="playButtonImage" 
+   @click="toggle_vid" id="play_button">
+  
+   <img src="images/volume.png" id="vol_img">
+   <input ref = "volumePlayer" type="range" id="change_vol" @click="change_volume" step="0.05" min="0" max="1" value="1">
   </div>
+ </div>
+</div>
+  </div>
+
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return{
+      isPlaying: false,
+      playButtonImage: require('@/assets/play.png'),
+      pauseButtonImage: require('@/assets/pause.png'),
+      currentTime: 0
+      
+    }
+  },
+  methods: {
+    toggle_vid(){
+      if(this.$refs.videoPlayer.paused)
+       {
+        this.isPlaying = true;
+        this.$refs.videoPlayer.play();
+       }
+       else{
+        this.$refs.videoPlayer.pause();
+        this.isPlaying = false;
+       }
+
+    },
+    change_volume(){
+      this.$refs.videoPlayer.volume= this.$refs.valuePlayer;
+    },
+    stop_video(){
+      this.$refs.videoPlayer.pause();
+      this.currentTime = 0;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+#wrapper
+{
+ text-align:center;
+ margin:0 auto;
+ padding:0px;
+ width:995px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#player_wrapper
+{
+ position:relative;
+ width:400px;
+ margin-left:300px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+#video_player
+{
+ width:400px;
 }
-a {
-  color: #42b983;
+#player_controls
+{
+ top:98%;
+ position:absolute;
+ background-color:black;
+ width:100%;
+ padding:5px;
+ box-sizing:border-box;
+}
+input[type="image"]
+{
+ float:left;
+ height:20px;
+ margin-left:2px;
+ margin-right:5px;
+ margin-top:2px;
+}
+#vol_img
+{
+ margin-left:150px;
+ width:20px;
 }
 </style>
